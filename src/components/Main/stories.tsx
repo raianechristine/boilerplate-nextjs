33679c000c5b257ac79bc8b6470ab72ad8447c9d
raiane <raianechristine@ioasys.com.br>
2020-10-20 14:44:49 -0300

    Removido addons e adicionado controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,17 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Main from '.';
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'NextJs, TypeScript, Styled Components')}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+
+Basic.args = {
+  title: 'title',
+  description: 'description'
+};
